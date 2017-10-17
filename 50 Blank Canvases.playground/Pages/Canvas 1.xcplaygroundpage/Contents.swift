a01914200df4eada7f525cd616efb3c1fbffbf5b
Mesburis, Cassie <cmesburis@lcs-mba3252.lcs.on.ca>
2017-10-17 09:59:11 -0400

    added with different colours and shapes

--- 50 Blank Canvases.playground/Pages/Canvas 1.xcplaygroundpage/Contents.swift
+++ 50 Blank Canvases.playground/Pages/Canvas 1.xcplaygroundpage/Contents.swift
@@ -39,23 +39,24 @@ for x in stride (from: 25, through: 475, by: 50) {
   let randomvalue = random(from: 1, toButNotIncluding: 5)
         
         if randomvalue == 1 {
-        canvas.fillColor = Color.black
+        canvas.fillColor = Color.init(hue: 150, saturation: 79, brightness: 69, alpha: 100)
         canvas.drawEllipse(centreX: x + 15, centreY: y, width: 25, height: 25)
     }
         if randomvalue == 2 {
-            canvas.fillColor = Color.black
+            canvas.fillColor = Color.init(hue: 12, saturation: 83, brightness: 37, alpha: 100)
             canvas.drawEllipse(centreX: x - 15, centreY: y, width: 25, height: 25)
         }
         
         if randomvalue == 3 {
-            canvas.fillColor = Color.black
+            canvas.fillColor = Color.init(hue: 201, saturation: 99, brightness: 98, alpha: 100)
             canvas.drawEllipse(centreX: x , centreY: y - 15, width: 25, height: 25)
         }
         
         if randomvalue == 4 {
    
-            canvas.fillColor = Color.black
-            canvas.drawEllipse(centreX: x , centreY: y + 15, width: 25, height: 25)
+            canvas.fillColor = Color.init(hue: 304, saturation: 58, brightness: 89, alpha: 100)
+            canvas.drawRectangle(bottomLeftX: x, bottomLeftY: y + 7, width: 20, height: 20)
+           
             }
 
 
