7c7a81f3b536d5381c9720419dad1d502de91aaf
Mesburis, Cassie <cmesburis@lcs-mba3252.lcs.on.ca>
2017-10-17 09:46:31 -0400

    finished circle project original. will work to add colour and shapes etc.

--- 50 Blank Canvases.playground/Pages/Canvas 1.xcplaygroundpage/Contents.swift
+++ 50 Blank Canvases.playground/Pages/Canvas 1.xcplaygroundpage/Contents.swift
@@ -9,6 +9,19 @@
  
  Please do not remove.
  */
+
+
+//: [Next](@next)
+//: # A Blank Canvas
+//:
+//: Use this page to experiment. Have fun!
+/*:
+ ## Required code
+ 
+ The following statements are required to make the playground run.
+ 
+ Please do not remove.
+ */
 import Cocoa
 import PlaygroundSupport
 
@@ -18,54 +31,41 @@ let canvas = Canvas(width: 500, height: 500)
 canvas.defaultBorderWidth = 7
 
 for x in stride (from: 25, through: 475, by: 50) {
-    canvas.fillColor = Color.white
     for y in stride(from: 475, through: 25, by: -50) {
+        // white
+        canvas.fillColor = Color.white
         canvas.drawEllipse(centreX: x, centreY: y, width: 25, height: 25)
+        
+  let randomvalue = random(from: 1, toButNotIncluding: 5)
+        
+        if randomvalue == 1 {
+        canvas.fillColor = Color.black
+        canvas.drawEllipse(centreX: x + 15, centreY: y, width: 25, height: 25)
     }
-    
-    let randomvalue = random(from: 1, toButNotIncluding: 5)
-    
-    if randomvalue == 1 { (canvas.drawEllipse(centreX: x, centreY: 35, width: 25, height: 25))
-    }
-    
+        if randomvalue == 2 {
+            canvas.fillColor = Color.black
+            canvas.drawEllipse(centreX: x - 15, centreY: y, width: 25, height: 25)
+        }
+        
+        if randomvalue == 3 {
+            canvas.fillColor = Color.black
+            canvas.drawEllipse(centreX: x , centreY: y - 15, width: 25, height: 25)
+        }
+        
+        if randomvalue == 4 {
+   
+            canvas.fillColor = Color.black
+            canvas.drawEllipse(centreX: x , centreY: y + 15, width: 25, height: 25)
+            }
+
+
+
+
+        }
+
+
+
+        // Don't remove the code below
+        PlaygroundPage.current.liveView = canvas.imageView
+
 }
-
-
-
-
-
-
-
-/*:
- ## Add your code below
- 
- Be sure to write human-readable code.
- 
- Use whitespace and comments as appropriate.
- */
-
-
-
-
-/*:
- ## Use source control
- To keep your work organized, and receive feedback, source control is a must.
- 
- Please commit and push your work often.
- 
- ![source_control](source_control.png "Source Control")
- */
-/*:
- ## Template code
- The code below is necessary to see results in the Assistant Editor at right.
- 
- Please do not remove.
- 
- If you don't see output, remember to show the Assistant Editor, and switch to Live View:
- 
- ![timeline](timeline.png "Timeline")
- */
-// Don't remove the code below
-PlaygroundPage.current.liveView = canvas.imageView
-
-
